ec883a9f054013ac4135b8b6ff74c671a3ed28a1
lixin <lixin@yuankan.net>
2019-04-24 15:01:12 +0800

    Add imshow function.

--- utils.py
+++ utils.py
@@ -6,15 +6,16 @@ import cv2
 
 # 定义分类器（正脸识别）
 HAAR_ALT = cv2.CascadeClassifier('./data/haarcascade_frontalface_alt.xml')
+# 还是下面这个好
 LBP_IMPROVED = cv2.CascadeClassifier('./data/lbpcascade_frontalface_improved.xml')
 
 
-def dbg(s):
-    print(s)
+def dbg(*args):
+    print(*args)
     sys.stdout.flush()
 
 
-def detect_multi_face(im, classifier=HAAR_ALT):
+def detect_multi_face(im, classifier=LBP_IMPROVED):
     if isinstance(im, str):
         im = cv2.imread(im)
     assert im is not None
@@ -34,9 +35,25 @@ def imcut(im):
     return im
 
 
+def imshow(im, faces):
+    for face in faces:
+        x, y, w, h = face
+        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imshow('im', im)
+
+
 if __name__ == '__main__':
-    fn = 'a.jpg'
+    import os
+
+    fn = 'w.jpg'
     r = detect_multi_face(fn)
     dbg(r)
     im = imcut(fn)
     dbg(im.shape)
+
+    path = './imgs_v2'
+    im = cv2.imread(fn)
+    faces = detect_multi_face(im, LBP_IMPROVED)
+    dbg(fn, faces)
+    imshow(im, faces)
+    cv2.waitKey()
